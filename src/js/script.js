ae37138bdcd9478bc3ca374678c5fc2bce2c2996
Stenio Eric <stenioeric@gmail.com>
2024-06-11 16:28:38 -0300

    Adicionado info dos repositórios do usuário

--- src/js/script.js
+++ src/js/script.js
@@ -3,23 +3,31 @@ async function user(userName) {
     return await response.json()
 }
 
+async function repos(userName) {
+    const  response = await fetch(`https://api.github.com/users/${userName}/repos`)
+    return await response.json()
+}
 
 function getUserProfile(userName){
     user(userName).then(userData => {
-        let userInfo = `<img src= "${userData.avatar_url}" alt = "Foto do perfil do usuário" />
+        let userInfo =  `</div> class = "info">
+                         <img src= "${userData.avatar_url}" alt = "Foto do perfil do usuário" />
                         <div class = "data">
                             <h1>${userData.name ?? 'Não possui nome cadastrado'} </h1>
                             <p>${userData.bio ?? 'Não possui bio cadastrada'}</p>
+                        </div>
                         </div>`
                             
         document.querySelector('.profile-data').innerHTML = userInfo
+        getUserRepositories(userName)
 
     })
 }
 
 document.getElementById('btn-search').addEventListener('click', () => {
-    const userName = document.getElementById('input-search').value
-    getUserProfile(userName)
+    const userName = document.getElementById('input-search').value;
+    getUserProfile(userName);
+    
 })
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
@@ -31,3 +39,20 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
         getUserProfile(userName)
     }
 })
+
+
+function getUserRepositories(userName) {
+    repos(userName).then(reposData => {
+
+        let repositoriesItems = "";
+        reposData.forEach(repo => {
+            repositoriesItems += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
+        });
+
+        document.querySelector('.profile-data').innerHTML += `<div class="repositories section">
+                                                                <h2>Repositórios</h2>
+                                                                <ul>${repositoriesItems}</ul>
+                                                            </div>`;
+
+    });
+}
