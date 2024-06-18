6a4bdd53d604597f0038b92058ecf48a6c65f1a9
Stenio Eric <stenioeric@gmail.com>
2024-06-18 20:55:30 -0300

    Atualizada exportação de dados do usuário

--- src/js/script.js
+++ src/js/script.js
@@ -5,24 +5,23 @@ import { screen } from '/src/js/objects/screen.js'
 
 
 
-async function getUserProfile(userName){
+async function getUserData(userName){
 
     const userResponse = await getUser(userName)
+    const repositoriesResponse = await getRepositories(userName)
+
     user.setInfo(userResponse)
-    console.log(user)
+    user.setRepositories(repositoriesResponse)
 
     screen.renderUser(user)
 
-    // getUser(userName).then(userData => {
-    //    
-    //     getUserRepositories(userName)
+    console.log(user)
 
-    // })
 }
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value;
-    getUserProfile(userName);
+    getUserData(userName);
     
 })
 
@@ -32,23 +31,7 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const EnterPressed = key === 13
 
     if (EnterPressed) {
-        getUserProfile(userName)
+        getUserData(userName)
     }
 })
 
-
-function getUserRepositories(userName) {
-    getRepositories(userName).then(reposData => {
-
-        let repositoriesItems = "";
-        reposData.forEach(repo => {
-            repositoriesItems += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
-        });
-
-        document.querySelector('.profile-data').innerHTML += `<div class="repositories section">
-                                                                <h2>Repositórios</h2>
-                                                                <ul>${repositoriesItems}</ul>
-                                                            </div>`;
-
-    });
-}
