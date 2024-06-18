40493f2d7f9391756060187eef2a2502220ac148
Stenio Eric <stenioeric@gmail.com>
2024-06-18 20:54:48 -0300

    Correção de erro

--- src/js/services/repositories.js
+++ src/js/services/repositories.js
@@ -1,10 +1,8 @@
-import { baseUrl,repoQuant } from '/src/js/variables.js'
-
+import { baseUrl, repoQuant } from '/src/js/variables.js';
 
 async function getRepositories(userName) {
-    const  response = await fetch(`${baseUrl}userName}/repos?per_page=${repoQuant}`)
-    return await response.json()
+    const response = await fetch(`${baseUrl}${userName}/repos?per_page=${repoQuant}`);
+    return await response.json();
 }
 
-
-export{getRepositories}
+export { getRepositories };
